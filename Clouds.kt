517db7e7b5b5bddbbb114141ce0d46def10e450d
Bharat549sa <81371017+Bharat549sa@users.noreply.github.com>
2023-01-02 14:05:20 -0500

    Add files via upload

---
+++ Clouds.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapi.models
+
+import java.io.Serializable
+
+
+
+data class Clouds(
+    val all: Int
+) : Serializable
---
+++ Main.kt
@@ -0,0 +1,4 @@
+package com.example.weatherapi.models
+
+class Main {
+}
